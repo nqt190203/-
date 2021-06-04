5b7a871f2f3cde74c3abaf3c5c7fcbb6b9482be4
nqt190203 <85324629+nqt190203@users.noreply.github.com>
2021-06-04 09:27:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/yokitainaka
+    messLink: 'https://www.youtube.com/watch?v=Aj3xwjn78wo
 }
